3ff4a64db141b8892cf692ecc0ba1165e99d3033
NHollander2 <nickhollander54@gmail.com>
2021-06-30 16:15:40 -0500

    added ProjectDL

---
+++ ProjectDL/IRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using ProjectModels;
+
+namespace ProjectDL
+{
+    public interface IRepository
+    {
+        List<Customer>GetCustomers();
+
+        Customer GetCustomer(Customer p_cust);
+        Customer AddCustomer(Customer p_cust);
+    }
+
+}
+
