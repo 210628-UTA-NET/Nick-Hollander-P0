3ac49ef5b5cd18a01087c332f8649b3f31104241
NHollander2 <nickhollander54@gmail.com>
2021-06-30 15:06:02 -0500

    More directory changes, added class defintions

---
+++ ProjectUI/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using ProjectModels;
+
+namespace ProjectUI{
+    class Program{
+        static void main(string[]args){
+            IMenu CustomerMenu = new CustMenu();
+            bool repeat = true;
+            MenuType currentMenu = MenuType.MainMenu;
+
+            while (repeat)
+            {
+                Console.Clear();
+                CustomerMenu.Menu;
+                currentMenu = CustomerMenu.YourChoice();
+
+                switch (currentMenu)
+                {
+                    case MenuType.MainMenu:
+                        CustomerMenu = new MainMenu();
+                        break;
+                    case MenuType.CustMenu:
+                        CustomerMenu = new CustMenu();
+                        break;
+                    default:
+                        Console.WriteLine("Can't process what you want, try again");
+                        break;
+                }
+            }
+        }
+    }
+}
