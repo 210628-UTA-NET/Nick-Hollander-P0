43d554228c8dce3654f98fc9521b651a876dbd97
NHollander2 <nickhollander54@gmail.com>
2021-07-01 11:45:03 -0500

    created AddCustomer UI

---
+++ ProjectUI/AddCustomerMenu.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Threading;
+using ProjectBL;
+using ProjectModels;
+
+namespace ProjectUI{
+    public class AddCustomerMenu{
+        private static Customer _newCust = new Customer();
+        private IProjectBL _custBL;
+        public AddCustomerMenu(ICustomerBL)
+    }
+}
